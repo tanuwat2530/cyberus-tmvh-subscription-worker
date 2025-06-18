fee32335b880c24f4f4fc3aecaad9212cade3197
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-06-18 20:21:56 +0700

    Insert uuid to table

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/google/uuid" // Import the UUID package
 	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -308,8 +309,9 @@ func threadWorker(id int, wg *sync.WaitGroup, jsonString string, rdb *redis.Clie
 
 	//defer wg.Done() // Mark this goroutine as done when it exits
 
+	insertId := uuid.New()
 	logEntry := tmvh_subscription_logs{
-		ID:            subscriptionData.RefId,
+		ID:            insertId,
 		Action:        subscriptionData.Action,
 		Code:          subscriptionData.Code,
 		CyberusReturn: subscriptionData.ReturnStatus,
